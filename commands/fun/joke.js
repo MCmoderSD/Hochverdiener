a9e65e48a6a02378611111faf76addef6de06b66
Rebix <78218707+Reebix@users.noreply.github.com>
2022-06-04 23:18:29 +0200

    jokes

---
+++ commands/fun/joke.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+var insults;
+
+module.exports = {
+    name: 'jokes',
+    aliases: ['j'],
+
+    execute(client, message) {
+        const fileName = 'jokes';
+        fs.readFile(fileName, 'utf8', (err, data) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            insults = data.split('\n')
+            if(message.mentions.members)
+                message.mentions.members.forEach(member => {
+
+                    message.channel.send(insults[Math.floor(Math.random() * insults.length)]);
+                });
+        });
+    },
+};
+
+
