93f9f4d4c2f70b530ae5cd831dc03022c261b652
MCmoderSD <MCmoderSD@web.de>
2022-06-04 22:42:05 +0200

    sexistscore

--- commands/fun/sexistscore.js
+++ commands/fun/sexistscore.js
@@ -12,11 +12,7 @@ module.exports = {
 
             let score = random.int(0, 100);
 
-        message.reply(`hat heute einen Sexistscore von ${score}`)
-
+            message.channel.send(`${message.mentions.members.first()} hat heute einen Sexistscore von ${score}%`)
         }
-
-
-
     },
 };
