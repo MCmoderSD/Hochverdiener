11212fbdc3a336a51924fcb571aa59de043d15f1
MCmoderSD <MCmoderSD@web.de>
2022-06-13 18:54:18 +0200

    clean

--- commands/admin/kicktalk.js
+++ commands/admin/kicktalk.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'kicktalk',
-
+    
     async execute(client, message, args) {
         message.member.voice.channel.members.forEach(member => {
             member.voice.disconnect();
