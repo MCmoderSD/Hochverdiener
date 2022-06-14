f9d433fe12f94ac341bc2b106a347c782ae3a84d
MCmoderSD <MCmoderSD@web.de>
2022-06-14 09:36:39 +0200

    help

--- commands/core/help.js
+++ commands/core/help.js
@@ -1,5 +1,5 @@
 const ms = require('ms');
-const tagCommands = '!fakt !insult !iq !joke !mobbing !sexist'
+const tagCommands = '!dicksize !fakt !insult !iq !joke !mobbing !sexist'
 const musicCommands = '!back !clear !loop !nowplaying !pause !play !progress !queue !resume !save !search !seek !shuffle !skip !stop !volume'
 const allCommands = `${tagCommands} ${musicCommands}`
 module.exports = {
