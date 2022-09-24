be64ca2b342076ba8b9f3db02010d7394a41e78f
MCmoderSD <mcmodersd@web.de>
2022-09-24 23:49:35 +0200

    help

--- commands/core/help.js
+++ commands/core/help.js
@@ -1,4 +1,6 @@
 const ms = require('ms');
+const adminCommands = '!ban !disconnect !kick !kicktalk !setlvl'
+const coreCommands = '!help !ping'
 const tagCommands = '!dicksize !fakt !insult !iq !joke !mobbing !sexist !zahl'
 const musicCommands = '!back !clear !loop !nowplaying !pause !play !progress !queue !resume !save !search !seek !shuffle !skip !stop !volume'
 const allCommands = `${tagCommands} ${musicCommands}`
