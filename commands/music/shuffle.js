08bfde4b36e90989dd0a438e1bf493727cea0ec5
MCmoderSD <mcmodersd@web.de>
2022-09-25 02:19:19 +0200

    fix

--- commands/music/shuffle.js
+++ commands/music/shuffle.js
@@ -4,7 +4,7 @@ module.exports = {
     utilisation: '{prefix}shuffle',
     voiceChannel: true,
 
-    execute: async function (client, message) {
+    async execute(client, message) {
         const queue = player.getQueue(message.guild.id);
 
         if (!queue || !queue.playing) return message.channel.send(`Geht nicht! ist ${message.author} dumm? ❌`);
@@ -12,7 +12,6 @@ module.exports = {
         if (!queue.tracks[0]) return message.channel.send(`Dikka ${message.author} nach dem Song ist Ende Gelände! ❌`);
 
         await queue.shuffle();
-
 
         return message.channel.send(`Ok **${queue.tracks.length}** song(s) wurden geshuffled! ✅`);
     },
