d3638f4ecd7d11675bd37a8bbb91d4663d3e8245
MCmoderSD <mcmodersd@web.de>
2022-10-01 22:16:17 +0200

    iq fixed 100%

--- commands/fun/iq.js
+++ commands/fun/iq.js
@@ -1,11 +1,21 @@
 const random = require('random');
+const { ApplicationCommandOptionType } = require("discord.js");
 module.exports = {
     name: 'iq',
-    description: 'Shows your iq',
+    description: 'Ich mach n IQ Test',
     voiceChannel: false,
+    options: [
+        {
+        name: 'user',
+        description: 'User für den du den IQ testen soll',
+        type: ApplicationCommandOptionType.User,
+        required: false,
+    }
+    ],
 
     async execute({ inter }) {
+        let user = inter.options.getUser('user') || inter.user;
         let iq = random.int(-10, 212);
-        inter.reply(`Du hast ${iq} IQ`);
+        inter.reply(`${user} hat einen IQ von ${iq}`);
     },
 };
