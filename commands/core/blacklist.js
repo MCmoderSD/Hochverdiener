feee34dfe9c60b75583ae6268a5911489960c368
Seraphin Berger <MCmoderSD@web.de>
2022-10-06 20:01:06 +0200

    Changes by Rebix

--- commands/core/blacklist.js
+++ commands/core/blacklist.js
@@ -42,65 +42,71 @@ module.exports = {
                         );
                     client.commands.map((command) => {
 
-                    if (command.blacklist || result.includes(command.name + ";")) {
-                        let label = command.name;
-                        if (result.includes(command.name + ";"))
-                            label += " (Blacklisted)";
-                        row.components[0].addOptions([{
-                            label: label,
-                            value: command.name,
-                            description: command.description,
-                        }]);
-                    }
+                        if (command.blacklist || result.includes(command.name + ";")) {
+                            let label = command.name;
+                            if (result.includes(command.name + ";"))
+                                label += " (Blacklisted)";
+                            row.components[0].addOptions([{
+                                label: label,
+                                value: command.name,
+                                description: command.description,
+                            }]);
+                        }
+                    });
+                    inter.reply({embeds: [getEmbed("Blacklisted Commands", result)], components: [row]});
+                    client.on('interactionCreate', async interaction => {
+                        if (!interaction.isSelectMenu()) return;
+                        if (interaction.customId === 'added_blacklistselect') {
+                            interaction.values.forEach((value) => {
+                                switchCommandStatus(client, interaction, value);
+                                if (result.includes(value + ";"))
+                                    result = result.replace(value + ";", "");
+                                else
+                                    result += value + ";";
+                            });
+                            interaction.update({
+                                components: [row2],
+                                embeds: [getEmbed("Blacklisted Commands", result)]
+                            });
+                        }
+                    });
+                    client.on('interactionCreate', async interaction => {
+                        if (!interaction.isButton()) return;
+                        if (interaction.customId === 'added_blacklistagain') {
+                            const row = new ActionRowBuilder()
+                                .addComponents(
+                                    new SelectMenuBuilder()
+                                        .setCustomId('added_blacklistselect')
+                                        .setPlaceholder('Kein Command Ausgewählt')
+                                        .setMinValues(1)
+                                );
+                            client.commands.map((command) => {
+                                if (command.blacklist || result.includes(command.name + ";")) {
+                                    let label = command.name;
+                                    if (result.includes(command.name + ";"))
+                                        label += " (Blacklisted)";
+                                    row.components[0].addOptions([{
+                                        label: label,
+                                        value: command.name,
+                                        description: command.description,
+                                    }]);
+                                }
+                            });
+                            interaction.update({components: [row], embeds: [getEmbed("Blacklisted Commands", result)]});
+                        }
+                    });
+                    client.on('interactionCreate', async interaction => {
+                        if (!interaction.isButton()) return;
+                        if (interaction.customId === 'added_blacklistexit') {
+                            interaction.update({components: [], embeds: [getEmbed("Blacklisted Commands", result)]});
+                        }
+                    });
                 });
-                inter.reply({embeds: [getEmbed("Blacklisted Commands", result)], components: [row]});
-                client.on('interactionCreate', async interaction => {
-                    if (!interaction.isSelectMenu()) return;
-                    if (interaction.customId === 'added_blacklistselect') {
-                        interaction.values.forEach((value) => {
-                            switchCommandStatus(client, interaction, value);
-                            if (result.includes(value + ";"))
-                                result = result.replace(value + ";", "");
-                            else
-                                result += value + ";";
-                        });
-                        interaction.update({components: [row2], embeds: [getEmbed("Blacklisted Commands", result)]});
-                    }
-                });
-                client.on('interactionCreate', async interaction => {
-                    if (!interaction.isButton()) return;
-                    if (interaction.customId === 'added_blacklistagain') {
-                        const row = new ActionRowBuilder()
-                            .addComponents(
-                                new SelectMenuBuilder()
-                                    .setCustomId('added_blacklistselect')
-                                    .setPlaceholder('Kein Command Ausgewählt')
-                                    .setMinValues(1)
-                            );
-                        client.commands.map((command) => {
-                            if (command.blacklist || result.includes(command.name + ";")) {
-                                let label = command.name;
-                                if (result.includes(command.name + ";"))
-                                    label += " (Blacklisted)";
-                                row.components[0].addOptions([{
-                                    label: label,
-                                    value: command.name,
-                                    description: command.description,
-                                }]);
-                            }
-                        });
-                        interaction.update({components: [row], embeds: [getEmbed("Blacklisted Commands", result)]});
-                    }
-                });
-                client.on('interactionCreate', async interaction => {
-                    if (!interaction.isButton()) return;
-                    if (interaction.customId === 'added_blacklistexit') {
-                        interaction.update({components: [], embeds: [getEmbed("Blacklisted Commands", result)]});
-                    }
-                });
-            });
-        }
-    },
+            }
+        } else {
+            inter.reply("Du benötigst die Rolle Hochverdiener-Admin um diesen Command zu nutzen");
+        };
+    }
 };
 
 function switchCommandStatus(client, inter, command, reply) {
