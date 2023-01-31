e6ef0001ac6328f4a626119960ed0a409a1e9d86
Seraphin Berger <MCmoderSD@web.de>
2023-01-31 15:18:23 +0100

    autorestart

--- commands/fun/insult.js
+++ commands/fun/insult.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const random = require('random');
+require('random');
 const { ApplicationCommandOptionType } = require("discord.js");
 
 module.exports = {
--- start.sh
+++ start.sh
@@ -1,1 +1,10 @@
-node main.js
+#!/bin/bash
+while true; do
+  ./Update.sh
+  node main.js
+  if [ $? -eq 0 ]; then
+    break
+  fi
+  echo "Restarting in 3 seconds..."
+  sleep 3
+done
