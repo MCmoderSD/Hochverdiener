47d8d1b7dfeab028d94b6a75babe263632c92d8d
Seraphin Berger <MCmoderSD@web.de>
2023-01-31 19:54:43 +0100

    server autorestart

---
+++ Hochverdiener-3.0/start.sh
@@ -0,0 +1,11 @@
+﻿#!/bin/bash
+while true; do
+  # ./Update.sh
+  dotnet Hochverdiener-3.0.dll
+  # shellcheck disable=SC2181
+  if [ $? -eq 0 ]; then
+    break
+  fi
+  echo "Restarting in 3 seconds..."
+  sleep 3
+done
