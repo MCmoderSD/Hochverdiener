4b4f9984e9f33085b02f1cae5250c811da67bde9
Rebix <timon@set-richter.de>
2023-02-01 22:21:12 +0100

    fixed

--- Hochverdiener-3.0/Commands/IQ.cs
+++ Hochverdiener-3.0/Commands/IQ.cs
@@ -7,27 +7,21 @@ namespace Hochverdiener_3._0.Commands;
 public class IQ : BaseCommand
 {
     public IQ() : base(
-        name: "IQ", 
-        aliases: null, 
-        description: "Misst den IQ eines Users", 
-        options: new[] {new SlashCommandOptionBuilder().WithName("user").WithDescription("Ich sag dir den IQ von einem User").WithType(ApplicationCommandOptionType.User).WithRequired(false)},
-        permission: null)
+        "IQ", 
+        null, 
+        "Misst den IQ eines Users", 
+        options: new[] {new SlashCommandOptionBuilder().WithName("user").WithDescription("User den du testen willst").WithType(ApplicationCommandOptionType.User).WithRequired(false)},
+            null)
     {
+        SlashCommandOptionBuilder option = new SlashCommandOptionBuilder();
     }
     
     public override Task Execute(SocketSlashCommand command)
     {
-        SocketGuildUser taggedUser;
-        SocketUser user = command.User;
-        if (command.Data.Options.First() != null)
-        {
-            taggedUser = (SocketGuildUser) command.Data.Options.First().Value;
-
-        }
-        
-        if(taggedUser != null) user = 
+        SocketGuildUser user = (SocketGuildUser) command.Data.Options.First().Value;
         var iq = new Random().Next(-12, 210);
         command.RespondAsync( user.Mention + " dein IQ ist " + iq);
+        Console.Write(user.Username);
         Console.WriteLine("IQ Command executed");
         return base.Execute(command);
     }
