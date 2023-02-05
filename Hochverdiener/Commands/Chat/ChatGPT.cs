ff3f5d66d2c0349a7afcfce2fa9458b912079ebb
Rebix <timon@set-richter.de>
2023-02-05 20:44:46 +0100

    added Ada / Davinci GPT option

--- Hochverdiener/Commands/Chat/ChatGPT.cs
+++ Hochverdiener/Commands/Chat/ChatGPT.cs
@@ -17,7 +17,7 @@ public class ChatGPT : BaseCommand
         name: "ChatGPT", 
         aliases: null, 
         description: "Ich beantworte deine Fragen mithilfe meines HochverdienerIQ's",
-        options: new[] {new SlashCommandOptionBuilder().WithName("question").WithDescription("Frage die du stellen willst").WithType(ApplicationCommandOptionType.String).WithRequired(true)},
+        options: new[] {new SlashCommandOptionBuilder().WithName("question").WithDescription("Frage die du stellen willst").WithType(ApplicationCommandOptionType.String).WithRequired(true), new SlashCommandOptionBuilder().WithName("questionquality").WithDescription("Schnell generiert oder langsam aber besser").WithRequired(true).WithType(ApplicationCommandOptionType.String).AddChoice("Schnell", "ada").AddChoice("Langsam", "davinci")},
         permission: null)
     {
         
@@ -25,15 +25,22 @@ public class ChatGPT : BaseCommand
     
     public override Task Execute(SocketSlashCommand command)
     {
+        Model model = command.Data.Options.Last().Value.ToString() == "ada" ? Model.AdaText : Model.DavinciText;
         var question = command.Data.Options.First().Value.ToString();
-        command.RespondAsync(AskChatGPT(question));
+        command.RespondAsync(AskChatGPT(question,model));
         return base.Execute(command);
     }
-    private string AskChatGPT(string? question)
+    private string AskChatGPT(string? question, Model model)
     {
-        var api = new OpenAIAPI(Keys.OpenAiApiKey);
+        //TODO: Conversations
+        string key;
+        if(model == Model.AdaText)
+            key = Keys.OpenAiApiKeyAdaText;
+        else
+            key = Keys.OpenAiApiKeyDavinciText;
+        var api = new OpenAIAPI(key);
         var result =
-            api.Completions.CreateAndFormatCompletion(new CompletionRequest(question, model: Model.DavinciText, temperature: 0.1));
+            api.Completions.CreateAndFormatCompletion(new CompletionRequest(question, model: model, temperature: 0.1));
         var res = result.Result;
         Console.WriteLine(res);
         return res;
